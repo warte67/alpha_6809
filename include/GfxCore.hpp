a7c630fe30c6fe404d612cbfe882964a13418570
warte67 <jayfaries@yahoo.com>
2024-03-31 11:59:12 -0700

    update
    video modes are too abundant... needs reducing

--- include/GfxCore.hpp
+++ include/GfxCore.hpp
@@ -34,4 +34,42 @@ class GfxCore : public IDevice
 
     private:
         SDL_Window* sdl_window = nullptr;
+
+
+    public:
+        struct GTIMING
+        {
+            Word Width;         // display width
+            Word Height;        // display height
+        };
+        struct GMODE 
+        {            
+            Byte Timing_index;  // 
+            Word Res_Width;     // number of pixels per column
+            Word Res_Height;    // number of pixels per row
+            Byte Pixel_Width;   // horizontal scan multiplier
+            Byte Pixel_Height;  // vertical scan multiplier           
+            // ... one byte wasted 
+        };
+
+    private:
+        std::vector<GTIMING> vec_timings;
+        std::vector<GMODE> vec_gmodes;
+        void _init_gmodes();
 };
+
+/******
+
+    Needed to track all of the graphics modes:
+
+        Res_Width       // number of pixels per column
+        Res_Height      // number of pixels per row
+        Pixel_Width     // horizontal scan multiplier
+        Pixel_Height    // vertical scan multiplier
+
+        // calculated at run time?
+        Timing_Width    // Res_Width * Pixel_Width
+        Timing_Height   // Res_Height * Pixel_Height
+
+
+ ******/
