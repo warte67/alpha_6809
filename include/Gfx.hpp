41b52910d5c5df15eeaef830745c22094b44f045
warte67 <jayfaries@yahoo.com>
2024-04-03 15:02:14 -0700

    UD

--- include/Gfx.hpp
+++ include/Gfx.hpp
@@ -51,7 +51,7 @@ class Gfx : public IDevice
     private:
 
         // internal registers (do these really need to be statics?)
-        inline static Byte s_gfx_mode   = 0x00;     // default: 3 = 320x200 text 
+        inline static Byte s_gfx_mode   = 0x03;     // default: 3 = 320x200 text 
         inline static Byte s_gfx_emu    = 0x00;     // default: 0 = windowed... primary monitor
         inline static Byte _gfx_pal_idx = 0x00;     // GFX_PAL_IDX
 
@@ -65,16 +65,37 @@ class Gfx : public IDevice
 
 
 /******
-
-   GFX_MODE     (Byte) Graphics Mode
-                - bit 0-4   = Resolution Modes 0-31
-                - bit 5-6   = 0:1bpp  1:2bpp  2:4bpp  3:8bpp
-                - bit 7     = 0:text,  1:bitmap
-
-    GFX_EMU     (Byte) Emulation Flags
-                - bits 0-2  = Active Monitor 0-7
-                - bits 3-5  = reserved
-                - bit  6    = 0:vsync off, 1:vsync on
-                - bit  7    = 0:windowed, 1:fullscreen
+              
+    GFX_MODE         = 0xFE00, // (Byte) Graphics Mode
+        //           - bit 0-4   = Resolution Modes 0-31
+        //           - bit 7     = 0:text,  1:bitmap
+        
+    GFX_EMU          = 0xFE01, // (Byte) Emulation Flags
+        //           - bits 0-2  = Active Monitor 0-7
+        //           - bits 3-5  = reserved
+        //           - bit  6    = 0:vsync off, 1:vsync on
+        //           - bit  7    = 0:windowed, 1:fullscreen
+        
+    GFX_PAL_IDX      = 0xFE02, //  (Byte) Color Palette Index
+        // GFX_PAL_IDX: 0-255
+        // Note: Use this register to set the index into theColor Palette. 
+        //   Set this value prior referencing color data (GFX_PAL_CLR).
+        
+    GFX_PAL_CLR      = 0xFE03, //  (Word) Indexed Color Palette Data
+        // GFX_PAL_CLR: Color Data A4R4G4B4 format
+        // Note: This is the color data for an individual palette entry.
+        //     Write to DSP_PAL_IDX with the index within the color palette
+        //     prior to reading or writing the color data in the GFX_PAL_CLR register.
+        
+    GFX_GLYPH_IDX    = 0xFE05, //  (Byte) Text Glyph Index
+        // GFX_GLYPH_IDX: 0-256
+        // Note: Set this register to index a specific text glyph. Set this value
+        //     prior to updating glyph pixel data.
+        
+    GFX_GLYPH_DATA   = 0xFE06, //  (8-Bytes) Text Glyph Pixel Data Array
+        // GFX_GLYPH_DATA: 8 rows of binary encoded glyph pixel data
+        // Note: Each 8x8 text glyph is composed of 8 bytes. The first byte in this 
+        //     array represents the top line of 8 pixels. Each array entry represents
+        //     a row of 8 pixels.                 
 
  ******/
