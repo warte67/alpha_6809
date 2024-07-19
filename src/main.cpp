1755e3581ea0043f61d6ffbde53cdb3624f97b27
warte67 <jayfaries@yahoo.com>
2024-07-19 13:33:51 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,14 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include "Bus.hpp"
-    
+
 int main(int argc, char *argv[])
 {
     Bus& bus = Bus::Inst();
     bus.Run();
-
     #ifdef DEBUG
         std::cout << "DEBUG flag was set at compile time.\n";
     #endif
-
     // std::cout << "\n\n\nProgram Terminated\n\n\n";
     return (int)0;
 }
